55b3db798e2cde7e8af295efbf41b08e7ad86552
Gohyo04 <gohyo7@gmail.com>
2024-02-04 02:24:04 +0900

    0204_chat_template_apply

--- src/main/webapp/resources/assets/js/chat/chat.js
+++ src/main/webapp/resources/assets/js/chat/chat.js
@@ -4,32 +4,41 @@
 
 const sendBtn = document.getElementById('sendBtn');
 const msgArea = document.getElementById('msgArea');
-let msg;
+const msgForm = document.getElementById('msgForm');
 
-console.log('why');
-sendBtn.addEventListener("click",()=>{
-    msg = document.getElementById('msg');
-    console.log('click');
-    sendMessage();
-    msg.value = '';
- });
+console.log("js");
+// console.log('why');
+// sendBtn.addEventListener("click",()=>{
+//     msg = document.getElementById('msg');
+//     console.log('click');
+//     sendMessage();
+//     msg.value = '';
+//  });
 // 
 let sock = new SockJS('http://localhost:80/chat');
-sock.onmessage = onMessage;
-sock.onOpen = onOpen;
-sock.onclose = onClose;
+// sock.onmessage = onMessage;
+// sock.onOpen = onOpen;
+// sock.onclose = onClose;
 
 sock.onOpen = e =>{
     msgArea.innerHTML = '';
 }
 
-sock.onmessage = e =>{
-    const chat = document.createElement('span');
+sock.onMessage = e =>{
+    let chat = document.createElement('span');
     chat.innerText = e.data;
+    
+    console.log(e.data);
     msgArea.appendChild(chat);
 }
 
-
+msgArea.onsubmit = e =>{
+    e.preventDefault();
+    sock.send(msgForm['msg'].value);
+    
+    msgForm['msg'].value = '';
+    msgForm['msg'].focus();
+}
 
 function sendMessage(){
     console.log('sendMessage');
