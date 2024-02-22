fd2eabced781e51dd4bde0041ad5f646fd6422f2
Noh <arark057@gmail.com>
2024-02-22 16:56:04 +0900

    qwe

--- src/main/java/com/somoim/app/moim/board/MoimReplyDAO.java
+++ src/main/java/com/somoim/app/moim/board/MoimReplyDAO.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ReplyDAO {
+public class MoimReplyDAO {
 	@Autowired
 	private SqlSession sqlSession;
 	private final String NAMESPACE ="com.somoim.app.moim.board.ReplyDAO.";
