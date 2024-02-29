5155944c34a1cc8722580e959e3353ba3197eca5
nammuung <audrbs2368@naver.com>
2024-02-29 14:42:42 +0900

    123

--- src/main/webapp/resources/js/admin/adminlist.js
+++ src/main/webapp/resources/js/admin/adminlist.js
@@ -3,8 +3,10 @@ moimList.addEventListener("click", function (e) {
     e.preventDefault();
     let width = 500;
     let height = 600;
-    let left = (window.innerWidth - width) / 2;
-    let top = (window.innerHeight - height) / 2;
+    let screenWidth = window.innerWidth;
+    let screenHeight = window.innerHeight;
+    let left = (screenWidth - width) / 2;
+    let top = (screenHeight - height) / 2;
     let features = `width=${width},height=${height},left=${left},top=${top}`;
 
     let popupWindow = window.open("/admin/adminList", "PopupWin", features);
@@ -12,7 +14,4 @@ moimList.addEventListener("click", function (e) {
         let content = popupWindow.document.querySelector("body");
         content.style.overflowX = "auto";
     });
-    // popupWindow.onresize = (_) => {
-    //     popupWindow.resizeTo(width, height);
-    // };
 });
