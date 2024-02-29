30eac4729eba32d31cab16180954d6d330b40f68
nammuung <audrbs2368@naver.com>
2024-02-29 15:31:22 +0900

    z

--- src/main/webapp/resources/js/admin/adminlist.js
+++ src/main/webapp/resources/js/admin/adminlist.js
@@ -1,18 +1,26 @@
 let moimList = document.getElementById("moimList");
+let popupWindow = null;
+let width = 1100;
+let height = 600;
+
 moimList.addEventListener("click", function (e) {
     e.preventDefault();
-    let width = 1100;
-    let height = 600;
     let left = (window.innerWidth - width) / 2;
     let top = (window.innerHeight - height) / 2;
     let features = `width=${width},height=${height},left=${left},top=${top}`;
 
-    let popupWindow = window.open("/admin/adminList", "PopupWin", features);
+    popupWindow = window.open("/admin/adminList", "PopupWin", features);
     popupWindow.addEventListener("load", () => {
         let content = popupWindow.document.querySelector("body");
         content.style.overflowX = "auto";
     });
-    popupWindow.onresize = (_) => {
-        popupWindow.resizeTo(width, height);
-    };
+});
+
+// 팝업 창의 크기 변경 시
+window.addEventListener("resize", function () {
+    if (popupWindow) {
+        let newWidth = Math.max(width, window.innerWidth);
+        let newHeight = Math.max(height, window.innerHeight);
+        popupWindow.resizeTo(newWidth, newHeight);
+    }
 });
